d083be7f798792268ea3f0b4ce6fc44212a436f5
arturo <arturogamez51@gmail.com>
2024-06-11 19:59:14 -0700

    adding mantine

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,23 @@
 import { Inter } from "next/font/google";
+import { MantineProvider } from "@mantine/core";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Developer 🚀 | Portfolio | Arturo Gamez ",
-  description: "Personal Portfolio to showcase my experience and project",
+    title: "Developer 🚀 | Portfolio | Arturo Gamez ",
+    description: "Personal Portfolio to showcase my experience and project",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    const theme = {
+        // Your custom colors, font families, etc.
+    };
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <MantineProvider theme={theme}>{children}</MantineProvider>
+            </body>
+        </html>
+    );
 }
