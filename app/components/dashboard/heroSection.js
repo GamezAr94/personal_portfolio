7781725d2a07cf038b391e3dcd1631207fb6b9a9
arturo <arturogamez51@gmail.com>
2024-06-16 13:15:42 -0700

    adding the 3d model with the animation and basic styles

--- app/components/dashboard/heroSection.js
+++ app/components/dashboard/heroSection.js
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import styles from "./heroSection.module.css";
 //import landscape from "@/public/img/hero_landscape.jpeg";
-import landscape from "@/public/img/hero_landscape_v2.jpeg";
+import landscape from "@/public/img/hero_landscape_v2-min.webp";
+import MainModel from "./mainModelTD";
 
 export default function HeroSection() {
     return (
         <div className={styles.hero_section_container}>
-            <div className={styles.model_3d}>hero</div>
             <Image
                 className={styles.landscape}
                 src={landscape}
-                alt="the screenshot of the dashboard page with the info of the account"
+                alt="a landscape that matches with out main character, a frog, it is portraying a red big mushroom"
                 priority={true}></Image>
+                <div className={styles.divider}></div>
+            <div className={styles.model_3d}>
+                <MainModel />
+            </div>
         </div>
     );
 }
