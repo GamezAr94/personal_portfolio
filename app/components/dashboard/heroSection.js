61831167256cc9936983ca36a86e7ca28d0302e9
arturo <arturogamez51@gmail.com>
2024-06-17 20:41:38 -0700

    adding the divider of the splash art

--- app/components/dashboard/heroSection.js
+++ app/components/dashboard/heroSection.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./heroSection.module.css";
-//import landscape from "@/public/img/hero_landscape.jpeg";
 import landscape from "@/public/img/hero_landscape_v2-min.webp";
 import MainModel from "./mainModelTD";
 
@@ -12,7 +11,7 @@ export default function HeroSection() {
                 src={landscape}
                 alt="a landscape that matches with out main character, a frog, it is portraying a red big mushroom"
                 priority={true}></Image>
-                <div className={styles.divider}></div>
+            <div className={styles.divider}></div>
             <div className={styles.model_3d}>
                 <MainModel />
             </div>
