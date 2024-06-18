e0207af8aba9dd3016571c554a8ab1ea8683c9e4
arturo <arturogamez51@gmail.com>
2024-06-17 21:30:03 -0700

    finishing the implementation of the 3d model

--- app/components/dashboard/mainModelTD.js
+++ app/components/dashboard/mainModelTD.js
@@ -1,18 +1,16 @@
 import Spline from "@splinetool/react-spline/next";
 import styles from "./mainModelTD.module.css";
 import { Suspense } from "react";
-import { isMobile } from "react-device-detect";
 
 export default function MainModel() {
     return (
         <Suspense
             fallback={<div className={styles.loading_element}>Loading...</div>}>
-            {isMobile ? <p>THIS IS MOBILE</p> : <p>NOT MOBILE!</p>}
             <Spline
                 className={styles.model}
                 width={738}
                 height={666}
-                scene="https://prod.spline.design/xUvQAzFB9FC1FWF8/scene.splinecode"
+                scene="https://draft.spline.design/MplG3TmNSKKdu65L/scene.splinecode"
             />
         </Suspense>
     );
