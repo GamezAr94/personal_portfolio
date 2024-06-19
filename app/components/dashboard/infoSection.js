9fe1b45be3d21a92297b36ed4b19b21cf228b58c
arturo <arturogamez51@gmail.com>
2024-06-18 23:11:46 -0700

    starting with the about me section

--- app/components/dashboard/infoSection.js
+++ app/components/dashboard/infoSection.js
@@ -4,7 +4,25 @@ export default function InfoSection() {
     return (
         <div className={styles.main_container}>
             <div className={styles.theme_picker}>hola</div>
-            <div className={styles.about_section}>hola</div>
+            <div className={styles.about_section}>
+                <div className={styles.about_title}>
+                    <h6>About</h6>
+                </div>
+                <div className={styles.about_container}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent vitae tincidunt enim, eget aliquam massa. Ut
+                        rutrum enim nisi, a semper erat condimentum et.
+                        Suspendisse faucibus libero turpis, suscipit imperdiet
+                        lectus euismod eu. Interdum et malesuada fames ac ante
+                        ipsum primis in faucibus. Nam sed justo eget orci
+                        dignissim posuere. Morbi pulvinar augue dui, ac lobortis
+                        mi accumsan sed. Sed nec massa at magna convallis
+                        tristique.
+                    </p>
+                </div>
+                <div className={styles.about_buttons}>holas</div>
+            </div>
             <div className={styles.skills}>
                 <h6>Skills</h6>
                 <div className={styles.skills_container}>
