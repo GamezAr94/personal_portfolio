7a162dd1c3c75652241bee3591049a7cf931eb13
arturo <arturogamez51@gmail.com>
2024-06-18 21:32:42 -0700

    finishing the styles for the stats section

--- app/components/dashboard/statsContainer.js
+++ app/components/dashboard/statsContainer.js
@@ -1,10 +1,26 @@
+import Link from "next/link";
 import styles from "./statsContainer.module.css";
 
 export default function StatsContainer() {
     return (
         <div className={styles.stats_container}>
-            <div className={styles.details}>l</div>
-            <div className={styles.links}>r</div>
+            <div className={styles.details}>
+                <div className={styles.top_expertise}>
+                    <p className={styles.year_xp}>
+                        3<span>+</span>
+                    </p>
+                    <p className={styles.year_string}>Years Of Experience</p>
+                </div>
+                <div className={styles.bottom_data}>
+                    <p>4 certificates</p>
+                    <p>3 published projects</p>
+                </div>
+            </div>
+            <div className={styles.links}>
+                <Link href="#">L</Link>
+                <Link href="#">G</Link>
+                <Link href="#">M</Link>
+            </div>
         </div>
     );
 }
