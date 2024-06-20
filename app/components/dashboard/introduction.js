ee2bb8340d71460b126338ccdc61287bd29f6ed9
arturo <arturogamez51@gmail.com>
2024-06-19 22:03:25 -0700

    adding the animation hover splash button

--- app/components/dashboard/introduction.js
+++ app/components/dashboard/introduction.js
@@ -14,7 +14,36 @@ export default function Introduction() {
                 <h1>Full Stack Software Developer.</h1>
                 <OpenToWorkTag size="small" />
             </div>
-            <Link href="#target-section" className={styles.contact_me}>Contact Me</Link>
+            <div className={styles.button_wrapper}>
+                <div className={styles.effect}>
+                    <span></span>
+                    <span></span>
+                </div>
+                <Link href="#target-section" className={styles.contact_me}>
+                    Contact Me
+                </Link>
+            </div>
+            <svg>
+                <defs>
+                    <filter id="contact_filter">
+                        <feGaussianBlur
+                            in="SourceGraphic"
+                            stdDeviation="10"
+                            result="name"
+                        />
+                        <feColorMatrix
+                            in="name"
+                            mode="matrix"
+                            values="1 0 0 0 0
+                                    0 1 0 0 0
+                                    0 0 1 0 0
+                                    0 0 0 30 -15 "
+                            result="aab"
+                        />
+                        <feBlend in="SourceGraphic" in2="aab" />
+                    </filter>
+                </defs>
+            </svg>
         </>
     );
 }
