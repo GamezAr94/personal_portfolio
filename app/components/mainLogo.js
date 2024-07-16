708380f32684dd5c65a34d9cfdebbd34c6208a7a
arturo <arturogamez51@gmail.com>
2024-07-15 22:07:20 -0700

    removing a piece of debug code

--- app/components/mainLogo.js
+++ app/components/mainLogo.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import styles from "./mainLogo.module.css";
 
 export default function MainLogo({style}) {
-    console.log(style);
     return (
         <Link className={`${styles.logo} ${styles[style]}`} href="#home-dashboard">
             AG
