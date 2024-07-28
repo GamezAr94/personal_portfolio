f6f73721ddcb5d7a7e1667093fe3f41857a362d2
arturo <arturogamez51@gmail.com>
2024-07-28 13:51:12 -0700

    fixing typo in description

--- app/components/about_section/innerDescription.js
+++ app/components/about_section/innerDescription.js
@@ -11,16 +11,16 @@ export default function InnerDescription() {
                 applications within Linux environments. I&apos;m passionate
                 about leveraging my expertise in TypeScript, React, Next.js and
                 Node.js to deliver high-quality solutions. In my current role as
-                a at LinuxMagic, I successfully implemented new features,
-                enhanced existing code, and optimized website performance by
-                11%. Additionally, by designing intuitive user interfaces, I
-                reduced support calls by 7%. I&apos;m proficient in various
-                development tools and methodologies, including Agile Scrum, unit
-                testing and PostgreSQL database management. I&apos;m actively
-                involved in open-source projects and enjoy building my own side
-                projects to experiment with new technologies. I&apos;d love to
-                hear about your project and see how we can collaborate to bring
-                your vision to life.
+                a front-end developer at LinuxMagic, I successfully implemented
+                new features, enhanced existing code, and optimized website
+                performance by 11%. Additionally, by designing intuitive user
+                interfaces, I reduced support calls by 7%. I&apos;m proficient
+                in various development tools and methodologies, including Agile
+                Scrum, unit testing and PostgreSQL database management. I&apos;m
+                actively involved in open-source projects and enjoy building my
+                own side projects to experiment with new technologies. I&apos;d
+                love to hear about your project and see how we can collaborate
+                to bring your vision to life.
             </p>
             <ButtonSplash />
         </div>
