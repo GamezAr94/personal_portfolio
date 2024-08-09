37eba20723b16656584416809961ed906bee9375
arturo <arturogamez51@gmail.com>
2024-08-08 21:59:40 -0700

    adding one more project to it

--- app/components/projects_section/projectsEntry.js
+++ app/components/projects_section/projectsEntry.js
@@ -3,10 +3,11 @@ import SingleProject from "./singleProject";
 import UnderDevelopment from "../underDevelopment";
 
 import cafelokal from "@/public/img/cafelokal/cafelokal_banner_short.gif";
-import astar from "@/public/img/astar_path_finding/astar_pathfinding_result.png";
+import astar from "@/public/img/astar_path_finding/astar_alg_gif.gif";
 import furelise from "@/public/img/furelise/fur_elise_banner_gif.gif";
 import lucygame from "@/public/img/lucyGame/lucyGame-gif.gif";
 import tamalyshop from "@/public/img/tamalyshop/tamalyshop_banner_gif.gif";
+import bentobox from "@/public/img/bento_layout/bento_layout_desktop.png";
 /*
 import me_v1 from "@/public/img/MyPictures/myself_v1.jpg";
 */
@@ -67,6 +68,17 @@ export default function ProjectsEntry() {
                     title="Tamaly Shop"
                     subtitle="UX/UI Design & Freelance Development"
                     image={tamalyshop}
+                />
+                <SingleProject
+                    title="Lucy Game"
+                    subtitle="Unity 2D, GameJam, Light and Collisions"
+                    swapped={true}
+                    image={lucygame}
+                />
+                <SingleProject
+                    title="Bento Box Layout"
+                    subtitle="Modern UX/UI Design - Experimenting"
+                    image={bentobox}
                 />
                 <SingleProject
                     title="Lucy Game"
