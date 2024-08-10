5e2a8f172daf36d61578e9c7b6fa97d28cc1bae8
arturo <arturogamez51@gmail.com>
2024-08-09 21:53:34 -0700

    adding more projects and setting imgs on mobile

--- app/components/projects_section/projectsEntry.js
+++ app/components/projects_section/projectsEntry.js
@@ -2,13 +2,21 @@ import styles from "./projectsEntry.module.css";
 import SingleProject from "./singleProject";
 import UnderDevelopment from "../underDevelopment";
 
-import cafelokal from "@/public/img/cafelokal/cafelokal_banner_short.gif";
-import astar from "@/public/img/astar_path_finding/astar_alg_gif.gif";
-import furelise from "@/public/img/furelise/fur_elise_banner_gif.gif";
-import lucygame from "@/public/img/lucyGame/lucyGame-gif.gif";
-import tamalyshop from "@/public/img/tamalyshop/tamalyshop_banner_gif.gif";
-import bentobox from "@/public/img/bento_layout/bento_layout_desktop.png";
-import froggame from "@/public/img/frogGame/frog_game_gameplay_gif.gif";
+import astar_sm from "@/public/img/astar_path_finding/astar_pathfinding_result.png";
+
+import cafelokal_sm from "@/public/img/cafelokal/cafelokal_banner.png";
+
+import furelise_sm from "@/public/img/furelise/fur_elise_banner.png";
+
+import lucygame_sm from "@/public/img/lucyGame/lucy_grab_light.png";
+
+import tamalyshop_sm from "@/public/img/tamalyshop/tamalyshop_banner.png";
+
+import bentobox_sm from "@/public/img/bento_layout/bento_layout_desktop.png";
+
+import froggame_sm from "@/public/img/frogGame/frog_main_menu_unity.png";
+
+import onlineIde_sm from "@/public/img/onlineIDE/more_code.png";
 /*
 import me_v1 from "@/public/img/MyPictures/myself_v1.jpg";
 */
@@ -46,46 +54,72 @@ export default function ProjectsEntry() {
                 <SingleProject
                     title="Cafe Lokal"
                     subtitle="UX/UI Design & Freelance Development"
-                    image={cafelokal}
+                    alt="banner of a coffe shop located at kitsilano vancouver"
+                    image={[
+                        "img/cafelokal/cafelokal_banner_short.gif",
+                        cafelokal_sm,
+                    ]}
                 />
                 <SingleProject
                     title="A* Path Finding Algorithm"
                     subtitle="Unity 2D, algorithm and games"
                     swapped={true}
-                    image={astar}
+                    alt="grid system to visualize the a* path finding algorithm in unity 2d"
+                    image={[
+                        "img/astar_path_finding/astar_alg_gif.gif",
+                        astar_sm,
+                    ]}
                 />
                 <SingleProject
                     title="Pâtisserie Für Elise"
                     subtitle="UX/UI Design & Freelance Development"
-                    image={furelise}
+                    alt="banner of a coffe shop located at downtown vancouver"
+                    image={[
+                        "img/furelise/fur_elise_banner_gif.gif",
+                        furelise_sm,
+                    ]}
                 />
                 <SingleProject
                     title="Lucy Game"
                     subtitle="Unity 2D, GameJam, Light and Collisions"
                     swapped={true}
-                    image={lucygame}
+                    alt="game made in unity of a firefly"
+                    image={["img/lucyGame/lucyGame-gif.gif", lucygame_sm]}
                 />
                 <SingleProject
                     title="Tamaly Shop"
                     subtitle="UX/UI Design & Freelance Development"
-                    image={tamalyshop}
+                    alt="banner of a mexican restaurant website located in commercial"
+                    image={[
+                        "img/tamalyshop/tamalyshop_banner_gif.gif",
+                        tamalyshop_sm,
+                    ]}
                 />
                 <SingleProject
-                    title="Lucy Game"
-                    subtitle="Unity 2D, GameJam, Light and Collisions"
+                    title="Online IDE"
+                    subtitle="Online IDE to solve Coding Problems"
                     swapped={true}
-                    image={lucygame}
+                    alt="interface of an online IDE to solve coding problems"
+                    image={["img/onlineIDE/full_video_gif.gif", onlineIde_sm]}
                 />
                 <SingleProject
                     title="Bento Box Layout"
                     subtitle="Modern UX/UI Design - Experimenting"
-                    image={bentobox}
+                    alt="banner of a modern website using a bento box layout"
+                    image={[
+                        "img/bento_layout/bento_layout_desktop.png",
+                        bentobox_sm,
+                    ]}
                 />
                 <SingleProject
                     title="Frog Game"
                     subtitle="Unity 2D, random position, force and speed"
                     swapped={true}
-                    image={froggame}
+                    alt="game of a frog chasing flies made with unity 2d"
+                    image={[
+                        "img/frogGame/frog_game_gameplay_gif.gif",
+                        froggame_sm,
+                    ]}
                 />
             </div>
             <UnderDevelopment />
--- app/components/projects_section/singleProject.js
+++ app/components/projects_section/singleProject.js
@@ -1,19 +1,31 @@
 import Image from "next/image";
 import styles from "./singleProject.module.css";
 
-export default function SingleProject({ swapped, title, subtitle, image }) {
+export default function SingleProject({
+    swapped,
+    title,
+    subtitle,
+    image,
+    alt,
+}) {
     return (
         <div
             className={`${styles.project_container} ${
                 swapped ? styles.swapped : ""
             }`}>
             <div className={styles.img_project}>
-                <Image
-                    className={`${styles.insta_photo}`}
-                    src={image}
-                    alt="a portrait of me in a mall in vancouver"
-                    priority={true}
-                />
+                <picture>
+                    <source
+                        media="(min-width: 600px)"
+                        srcSet={image[0]}></source>
+                    <Image
+                        className={`${styles.insta_photo} gif`}
+                        src={image[1]}
+                        alt={alt}
+                        loading="lazy"
+                        unoptimized
+                    />
+                </picture>
             </div>
             <div className={styles.desc_project}>
                 <p className={styles.title_project}>{title}</p>
