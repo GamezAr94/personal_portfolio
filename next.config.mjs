0ed4b15b420eba978e97d25ba2e78b363c33bae9
Arturo Gamez <arturogamez51@gmail.com>
2025-10-30 21:16:42 -0700

    starting a new project from zero using the default configuration

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        MY_EMAIL: "contact@arturogamez.com",
-        OPEN_TO_WORK: "true", // Boolean values are supported
-    },
-};
-
-export default nextConfig;
