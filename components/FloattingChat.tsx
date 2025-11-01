d49388b0a8dd3331ced6142e1673356e719caaa2
Arturo Gamez <arturogamez51@gmail.com>
2025-11-01 12:30:01 -0700

    finishing the styles of the floating bar on closed state for mobile and desktop

--- components/FloattingChat.tsx
+++ components/FloattingChat.tsx
@@ -25,21 +25,6 @@ const ArrowIcon = () => (
         />
     </svg>
 );
-const ChatIcon = () => (
-    <svg
-        className={styles.chatIcon}
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-    >
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-        />
-    </svg>
-);
 const MobileArrowIcon = () => (
     <svg
         className={styles.icon}
@@ -92,8 +77,12 @@ export default function FloatingChat() {
                 <div style={{ cursor: 'pointer' }}>
                     <ArrowIcon />
                 </div>
-                <div style={{ cursor: 'pointer' }}>
-                    <ChatIcon />
+                <div className={styles.mainIconContainer}>
+                    <div className={styles.mainIconGradient}>
+                        {/* We're using the "A" from your code */}
+                        <span className={styles.mainIconLetter}>A</span>
+                    </div>
+                    <span className={styles.iconText}>AI Assistant</span>
                 </div>
                 <div></div> {/* Spacer */}
             </div>
