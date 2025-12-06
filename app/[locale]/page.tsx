90d618285657e58968c85637ab78a3067a8d475b
Arturo Gamez <arturogamez51@gmail.com>
2025-12-06 10:28:20 -0800

    adding the github shield for the projects that are from github

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -114,16 +114,28 @@ export default async function Home({ params }: { params: { locale: string } }) {
     };
     // Put your image paths from the /public folder here, in order (p1 to p6).
     const playgroundImageUrls = [
-        "/img/projects/portfolio_hero_screenshot.png",
-        "/img/projects/dkim_diagram.png", // p1
-        "/img/playground/raspberry-pi.jpg", // p2
-        "/img/projects/automated_spam_representation_rayso.png", // p1
-        "/img/projects/MFA_representation_rayso.png", // p1
-        "/img/projects/IDE_banner.png", // p1
-        "/img/projects/astar_pathfinding_result.png", // p1
-        "/img/projects/cafelokal_banner.jpg", // p1
-        //"/img/projects/lucy_game.jpg", // p1
-        "/img/projects/frog_game.jpg", // p1
+        "/img/projects/portfolio_hero_screenshot.png", //p1
+        "/img/projects/dkim_diagram.png", // p2
+        "/img/playground/raspberry-pi.jpg", // p3
+        "/img/projects/automated_spam_representation_rayso.png", // p4
+        "/img/projects/MFA_representation_rayso.png", // p5
+        "/img/projects/IDE_banner.png", // p6
+        "/img/projects/astar_pathfinding_result.png", // p7
+        "/img/projects/cafelokal_banner.jpg", // p8
+        //"/img/projects/lucy_game.jpg", // p9
+        "/img/projects/frog_game.jpg", // p9
+    ];
+    const linksToProject = [
+        "https://github.com/GamezAr94/personal_portfolio", //p1
+        null, // p2
+        null, // p3
+        null, // p4
+        null, // p5
+        "/img/projects/IDE_banner.png", // p6
+        "https://github.com/GamezAr94/AstarPathFindingAlgorithm", // p7
+        null, // p8
+        //"/img/projects/lucy_game.jpg", // p9
+        "https://github.com/GamezAr94/Frog-Game", // p9
     ];
     const playgroundProjects = Array.from(
         { length: PLAYGROUND_PROJECT_COUNT },
@@ -132,7 +144,8 @@ export default async function Home({ params }: { params: { locale: string } }) {
             return {
                 title: t_playground(`p${index}_title`),
                 description: t_playground(`p${index}_desc`),
-                imageUrl: playgroundImageUrls[i], // TODO: Add your image URLs
+                imageUrl: playgroundImageUrls[i],
+                linksGit: linksToProject[i],
                 imageAlt: t_playground(`p${index}_title`),
                 aiQuery: t_chat_question(`q_playground_${index}`),
             };
